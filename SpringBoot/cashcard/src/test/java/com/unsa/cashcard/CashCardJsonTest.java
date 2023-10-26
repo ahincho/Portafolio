6f0a1fc8cd57af206c49df044fffc147ebbe2b78
ahincho <ahincho@unsa.edu.pe>
2023-10-26 13:20:10 -0500

    Test for Get List Endpoint

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashCardJsonTest.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashCardJsonTest.java
@@ -2,7 +2,11 @@ package com.unsa.cashcard;
 
 import com.unsa.cashcard.domain.CashCard;
 
+import org.assertj.core.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -17,6 +21,20 @@ public class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.00),
+                new CashCard(101L, 150.00)
+        );
+    }
 
     @Test
     public void cashCardSerializationTest() throws IOException {
@@ -39,6 +57,11 @@ public class CashCardJsonTest {
         assertThat(json.parse(expected)).isEqualTo(new CashCard(99L, 123.45));
         assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson(new ClassPathResource("list.json"));
     }
 
 }
