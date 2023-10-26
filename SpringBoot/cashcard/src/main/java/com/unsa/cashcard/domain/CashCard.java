fcf64d49466d266b2fbc354093822928c54ac617
ahincho <ahincho@unsa.edu.pe>
2023-10-25 22:15:04 -0500

    Cash Card Class

---
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/domain/CashCard.java
@@ -0,0 +1,5 @@
+package com.unsa.cashcard.domain;
+
+public record CashCard(Long id, Double amount) {
+
+}
--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -1,11 +1,13 @@
 package com.unsa.cashcard;
 
+import com.unsa.cashcard.domain.CashCard;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
