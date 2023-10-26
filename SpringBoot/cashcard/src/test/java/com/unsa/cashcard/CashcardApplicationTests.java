cdcb2c028f541d78833c8ac102879aea976ac767
ahincho <ahincho@unsa.edu.pe>
2023-10-25 22:02:55 -0500

    First Unit Test on Spring Boot

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -3,11 +3,14 @@ package com.unsa.cashcard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class CashcardApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void myFirstTest() {
+		assertThat(1).isEqualTo(42);
 	}
 
 }
