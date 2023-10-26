e26f3b8aa075a1ed1870537ab8c72fff4b3707d8
ahincho <ahincho@unsa.edu.pe>
2023-10-25 23:45:43 -0500

    Updating Get Endpoint Test and Checking the Data

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -1,6 +1,10 @@
 package com.unsa.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -22,6 +26,9 @@ class CashcardApplicationTests {
 	void shouldReturnACashCardWhenDataIsSaved() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isNotNull();
 	}
 
 }
