389b1621db301101494a65792b8c8f25769254f4
ahincho <ahincho@unsa.edu.pe>
2023-10-25 23:43:38 -0500

    Implementing Get Endpoint

--- SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
@@ -1,9 +1,15 @@
 package com.unsa.cashcard.infrastructure;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/cashcards")
 public class CashCardController {
 
+    @GetMapping("/{requestedId}")
     public ResponseEntity<String> findById() {
         return ResponseEntity.ok("{}");
     }
