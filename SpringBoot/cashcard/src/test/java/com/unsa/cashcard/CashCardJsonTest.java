0231cc84d09b7dc593cdb11768dfdfa12b02f197
ahincho <ahincho@unsa.edu.pe>
2023-10-25 23:34:23 -0500

    JSON Deserialization Unit Test for Cash Card

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashCardJsonTest.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashCardJsonTest.java
@@ -28,4 +28,17 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
     }
 
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(1000L, 67.89));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1000);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
+    }
+    
 }
