86e580bc1b51e7e141adc7b34353b6a8decdca22
ahincho <ahincho@unsa.edu.pe>
2023-10-25 23:40:17 -0500

    Test to Get Data from Controller

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -1,20 +1,27 @@
 package com.unsa.cashcard;
 
-import com.unsa.cashcard.domain.CashCard;
-
-import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() { }
+
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 
 }
