dfd2fac3b418cb3becf8c0403ec8730978317afa
ahincho <ahincho@unsa.edu.pe>
2023-10-26 01:16:28 -0500

    Test for Post Endpoint

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -3,6 +3,7 @@ package com.unsa.cashcard;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
+import com.unsa.cashcard.domain.CashCard;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ class CashcardApplicationTests {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateANewCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.00);
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
