e08d0181e89352c5d8e50f35c687aee2d233e669
ahincho <ahincho@unsa.edu.pe>
2023-10-26 00:13:05 -0500

    Find Cash Card by Id from Repository

--- SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
@@ -21,9 +23,9 @@ public class CashCardController {
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
