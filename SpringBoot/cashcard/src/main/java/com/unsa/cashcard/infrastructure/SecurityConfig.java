7cc46142e75a4530ecbefea707a54807131bc678
ahincho <ahincho@unsa.edu.pe>
2023-10-26 16:49:13 -0500

    Security Configuration Bean

---
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.unsa.cashcard.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.build();
+    }
+
+}
