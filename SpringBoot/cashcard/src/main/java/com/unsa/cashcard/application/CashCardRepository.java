80c07ad939fde4091ff246d3f575c9bd6fcd6016
ahincho <ahincho@unsa.edu.pe>
2023-10-26 18:07:06 -0500

    Repositories Queries for Id and Owners

--- SpringBoot/cashcard/src/main/java/com/unsa/cashcard/application/CashCardRepository.java
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/application/CashCardRepository.java
@@ -1,9 +1,14 @@
 package com.unsa.cashcard.application;
 
 import com.unsa.cashcard.domain.CashCard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+
+    CashCard findByIdAndOwner(Long id, String owner);
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
 
 }
