d1e16b1cd8d8f3a5aa76595ce6a59faaf21a52d2
ahincho <ahincho@unsa.edu.pe>
2023-10-25 23:55:49 -0500

    Adding Path Variable at Get Endpoint

--- SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
+++ SpringBoot/cashcard/src/main/java/com/unsa/cashcard/infrastructure/CashCardController.java
@@ -4,6 +4,7 @@ import com.unsa.cashcard.domain.CashCard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CashCardController {
 
     @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
