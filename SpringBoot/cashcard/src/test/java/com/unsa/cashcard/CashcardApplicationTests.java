5f1f6a3d3f1e707d702889299e8d0f7e92d8197f
ahincho <ahincho@unsa.edu.pe>
2023-10-26 22:08:06 -0500

    Trying to Delete a Non Created Card

--- SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
+++ SpringBoot/cashcard/src/test/java/com/unsa/cashcard/CashcardApplicationTests.java
@@ -198,4 +198,12 @@ class CashcardApplicationTests {
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("sarah1", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+
 }
