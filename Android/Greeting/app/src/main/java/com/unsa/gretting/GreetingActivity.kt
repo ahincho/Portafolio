895797fbf081c96fbd9fdc93695bcbb55f9488c0
ahincho <ahincho@unsa.edu.pe>
2023-11-23 13:04:23 -0500

    From to Recover Name with Constraint Layout

--- Android/Greeting/app/src/main/java/com/unsa/gretting/GreetingActivity.kt
+++ Android/Greeting/app/src/main/java/com/unsa/gretting/GreetingActivity.kt
@@ -1,11 +1,22 @@
 package com.unsa.gretting
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import android.os.Bundle
+import android.util.Log
 
 class GreetingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gretting)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+            if (name.isNotEmpty()) {
+                Log.i("Angel", "Button Pressed: ${name}")
+            }
+        }
     }
 }
