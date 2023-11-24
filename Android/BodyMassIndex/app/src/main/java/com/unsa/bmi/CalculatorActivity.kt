94fdf386ca2be8eb0f8ed34b4c1222d7b78327b7
ahincho <ahincho@unsa.edu.pe>
2023-11-24 12:43:30 -0500

    Weight and Age Controls with Linear Layouts

--- Android/BodyMassIndex/app/src/main/java/com/unsa/bmi/CalculatorActivity.kt
+++ Android/BodyMassIndex/app/src/main/java/com/unsa/bmi/CalculatorActivity.kt
@@ -5,16 +5,25 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
 class CalculatorActivity : AppCompatActivity() {
     private var isMaleSelected : Boolean = true
     private var isFemaleSelected : Boolean = false
+    private var currentWeight : Int = 50
+    private var currentAge : Int = 20
     private lateinit var cardMale : CardView
     private lateinit var cardFemale : CardView
     private lateinit var tvHeight : TextView
     private lateinit var rsHeight : RangeSlider
+    private lateinit var btnSubtractWeight : FloatingActionButton
+    private lateinit var btnAddWeight : FloatingActionButton
+    private lateinit var tvWeight : TextView
+    private lateinit var btnSubtractAge : FloatingActionButton
+    private lateinit var btnAddAge : FloatingActionButton
+    private lateinit var tvAge : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
@@ -27,6 +36,12 @@ class CalculatorActivity : AppCompatActivity() {
         cardFemale = findViewById(R.id.cardFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnAddWeight = findViewById(R.id.btnAddWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnAddAge = findViewById(R.id.btnAddAge)
+        tvAge = findViewById(R.id.tvAge)
     }
     private fun initListeners() {
         cardMale.setOnClickListener {
@@ -42,6 +57,27 @@ class CalculatorActivity : AppCompatActivity() {
             val res = df.format(value)
             "${res}cm".also { tvHeight.text = it }
         }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1
+            updateWeight()
+        }
+        btnAddWeight.setOnClickListener {
+            currentWeight += 1
+            updateWeight()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1
+            updateAge()
+        }
+        btnAddAge.setOnClickListener {
+            currentAge += 1
+            updateAge()
+        }
+    }
+    private fun initUserInterface() {
+        setGenderColor()
+        updateWeight()
+        updateAge()
     }
     private fun setGenderColor() {
         cardMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
@@ -59,7 +95,10 @@ class CalculatorActivity : AppCompatActivity() {
         isMaleSelected = !isMaleSelected
         isFemaleSelected = !isFemaleSelected
     }
-    private fun initUserInterface() {
-        setGenderColor()
+    private fun updateWeight() {
+        tvWeight.text = currentWeight.toString()
+    }
+    private fun updateAge() {
+        "${this.currentAge} years".also { tvAge.text = it }
     }
 }
