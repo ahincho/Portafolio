dc7eb8e049ee4f1a1499b46fa65fd442ce7ef837
ahincho <ahincho@unsa.edu.pe>
2023-11-25 18:36:55 -0500

    List of Super Heroes

---
+++ Android/SuperHeroes/app/src/main/java/com/unsa/heroes/SuperHeroProvider.kt
@@ -0,0 +1,56 @@
+package com.unsa.heroes
+
+class SuperHeroProvider {
+    companion object {
+        var superHeroes : List<SuperHero> = listOf (
+            SuperHero (
+                "Spiderman",
+                "Marvel",
+                "Peter Parker",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"
+            ),
+            SuperHero (
+                "Daredevil",
+                "Marvel",
+                "Matthew Michael Murdock",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"
+            ),
+            SuperHero (
+                "Wolverine",
+                "Marvel",
+                "James Howlett",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
+            ),
+            SuperHero (
+                "Batman",
+                "DC Comics",
+                "Bruce Wayne",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"
+            ),
+            SuperHero (
+                "Thor",
+                "Marvel",
+                "Thor Odinson",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"
+            ),
+            SuperHero (
+                "Flash",
+                "DC Comics",
+                "Jay Garrick",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"
+            ),
+            SuperHero (
+                "Green Lantern",
+                "DC Comics",
+                "Alan Scott",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
+            ),
+            SuperHero (
+                "Wonder Woman",
+                "DC Comics",
+                "Princess Diana",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
+            )
+        )
+    }
+}
