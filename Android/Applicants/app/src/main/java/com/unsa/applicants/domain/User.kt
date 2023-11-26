add866abddcefdf19ad7258402f3364312d01777
ahincho <ahincho@unsa.edu.pe>
2023-11-26 11:26:11 -0500

    Override User Equals and HashCode Methods

--- Android/Applicants/app/src/main/java/com/unsa/applicants/domain/User.kt
+++ Android/Applicants/app/src/main/java/com/unsa/applicants/domain/User.kt
@@ -1,6 +1,17 @@
 package com.unsa.applicants.domain
 
-data class User (
-    val name: String,
-    val password: String
-)
+data class User( val name: String, val password: String) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as User
+        if (name != other.name) return false
+        if (password != other.password) return false
+        return true
+    }
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + password.hashCode()
+        return result
+    }
+}
