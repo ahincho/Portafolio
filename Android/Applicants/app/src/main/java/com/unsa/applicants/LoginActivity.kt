8cf3ff2daafd4bf553cbdcab0d3aa6f54630cb51
ahincho <ahincho@unsa.edu.pe>
2023-11-26 11:27:08 -0500

    Renaming Edit Text to Login Name

--- Android/Applicants/app/src/main/java/com/unsa/applicants/LoginActivity.kt
+++ Android/Applicants/app/src/main/java/com/unsa/applicants/LoginActivity.kt
@@ -2,11 +2,30 @@ package com.unsa.applicants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.unsa.applicants.databinding.ActivityLoginBinding
+import com.unsa.applicants.domain.User
+import com.unsa.applicants.providers.UserProvider
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+    }
+    private fun initListeners() {
+        binding.btnLogin.setOnClickListener {
+            onCheckCredentials()
+        }
+    }
+    private fun onCheckCredentials(): Boolean {
+        if (binding.etLoginName.text.isNotEmpty() && binding.etLoginPassword.text.isNotEmpty()) {
+            val user = User(binding.etLoginName.text.toString(), binding.etLoginPassword.text.toString())
+            Log.i("Angel", "Login: ${UserProvider.users.contains(user)}")
+            return UserProvider.users.contains(user)
+        }
+        return false
     }
 }
