c7994da0447a035669f14a63970be58f31fbb8de
ahincho <ahincho@unsa.edu.pe>
2023-11-26 17:38:08 -0500

    Menu Activity and Refactoring Layouts

---
+++ Android/Applicants/app/src/main/java/com/unsa/applicants/MenuActivity.kt
@@ -0,0 +1,22 @@
+package com.unsa.applicants
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unsa.applicants.databinding.ActivityMenuBinding
+
+class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        initListeners()
+    }
+    private fun initListeners() {
+        binding.btnApplicantList.setOnClickListener {
+            intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
