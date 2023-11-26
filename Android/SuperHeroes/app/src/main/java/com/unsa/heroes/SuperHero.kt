272c96e58af31c360832676201d91290609bf9b4
ahincho <ahincho@unsa.edu.pe>
2023-11-25 19:21:17 -0500

    Fixing Publisher and Name of Super Heroes

--- Android/SuperHeroes/app/src/main/java/com/unsa/heroes/SuperHero.kt
+++ Android/SuperHeroes/app/src/main/java/com/unsa/heroes/SuperHero.kt
@@ -1,8 +1,8 @@
 package com.unsa.heroes
 
 data class SuperHero (
-    val alias : String,
-    val name : String,
-    val publisher : String,
-    val photo : String
+    val alias: String,
+    val name: String,
+    val publisher: String,
+    val photo: String
 )
--- Android/SuperHeroes/app/src/main/java/com/unsa/heroes/SuperHeroProvider.kt
+++ Android/SuperHeroes/app/src/main/java/com/unsa/heroes/SuperHeroProvider.kt
@@ -2,53 +2,53 @@ package com.unsa.heroes
 
 class SuperHeroProvider {
     companion object {
-        var superHeroes : List<SuperHero> = listOf (
+        var superHeroes: List<SuperHero> = listOf (
             SuperHero (
                 "Spiderman",
-                "Marvel",
                 "Peter Parker",
+                "Marvel",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"
             ),
             SuperHero (
                 "Daredevil",
-                "Marvel",
                 "Matthew Michael Murdock",
+                "Marvel",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/daredevil.jpg"
             ),
             SuperHero (
                 "Wolverine",
-                "Marvel",
                 "James Howlett",
+                "Marvel",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
             ),
             SuperHero (
                 "Batman",
-                "DC Comics",
                 "Bruce Wayne",
+                "DC Comics",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"
             ),
             SuperHero (
                 "Thor",
-                "Marvel",
                 "Thor Odinson",
+                "Marvel",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"
             ),
             SuperHero (
                 "Flash",
-                "DC Comics",
                 "Jay Garrick",
+                "DC Comics",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/flash.png"
             ),
             SuperHero (
                 "Green Lantern",
-                "DC Comics",
                 "Alan Scott",
+                "DC Comics",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
             ),
             SuperHero (
                 "Wonder Woman",
-                "DC Comics",
                 "Princess Diana",
+                "DC Comics",
                 "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
             )
         )
