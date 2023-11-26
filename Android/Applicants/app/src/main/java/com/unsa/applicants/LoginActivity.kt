94e34141ac0c4be1b1e439b676f11ecd19e1d91e
ahincho <ahincho@unsa.edu.pe>
2023-11-26 10:28:31 -0500

    Login Interface and SVG Image

--- Android/Applicants/app/src/main/java/com/unsa/applicants/LoginActivity.kt
+++ Android/Applicants/app/src/main/java/com/unsa/applicants/LoginActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
