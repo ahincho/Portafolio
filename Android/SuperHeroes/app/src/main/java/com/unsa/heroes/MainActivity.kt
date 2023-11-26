965542109b5aa6b3d9c221b903557059987a9e49
ahincho <ahincho@unsa.edu.pe>
2023-11-25 19:22:49 -0500

    Loding Super Heroes Recycler View on Main Activity

--- Android/SuperHeroes/app/src/main/java/com/unsa/heroes/MainActivity.kt
+++ Android/SuperHeroes/app/src/main/java/com/unsa/heroes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.unsa.heroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.unsa.heroes.adapter.SuperHeroAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+    }
+    private fun initRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.rvSuperHeroes)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroes)
     }
 }
