4dd8fddd74af272358390d55753b78eadf2b4f13
ahincho <ahincho@unsa.edu.pe>
2023-12-07 07:44:23 -0500

    Navigation Menu to Fragments

--- Android/Fragments/app/src/main/java/com/unsa/fragments/MainActivity.kt
+++ Android/Fragments/app/src/main/java/com/unsa/fragments/MainActivity.kt
@@ -4,11 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navigation: BottomNavigationView
+    private val onNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.firstItem -> {
+                supportFragmentManager.commit {
+                    replace<FirstFragment>(R.id.flContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.secondItem -> {
+                supportFragmentManager.commit {
+                    replace<SecondFragment>(R.id.flContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.thirdItem -> {
+                supportFragmentManager.commit {
+                    replace<ThirdFragment>(R.id.flContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation = findViewById(R.id.bnvMenu)
+        navigation.setOnNavigationItemSelectedListener(onNavMenu)
         supportFragmentManager.commit {
             replace<FirstFragment>(R.id.flContainer)
             setReorderingAllowed(true)
