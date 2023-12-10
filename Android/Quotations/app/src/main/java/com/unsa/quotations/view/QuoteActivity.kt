907f51375a1bf006a2bebfe89bad426760ba8699
ahincho <ahincho@unsa.edu.pe>
2023-12-10 12:41:51 -0500

    ViewModel and Model Comms

---
+++ Android/Quotations/app/src/main/java/com/unsa/quotations/view/QuoteActivity.kt
@@ -0,0 +1,26 @@
+package com.unsa.quotations.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.unsa.quotations.R
+import com.unsa.quotations.databinding.ActivityQuoteBinding
+import com.unsa.quotations.viewmodel.QuoteViewModel
+
+class QuoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityQuoteBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityQuoteBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_quote)
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+        binding.vwContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
