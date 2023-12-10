6c6193b256c06266c571cf835e08a9e33e6d9075
ahincho <ahincho@unsa.edu.pe>
2023-12-10 01:20:24 -0500

    Add Task Button

--- Android/ToDoList/app/src/main/java/com/unsa/list/ListActivity.kt
+++ Android/ToDoList/app/src/main/java/com/unsa/list/ListActivity.kt
@@ -1,9 +1,11 @@
 package com.unsa.list
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListActivity : AppCompatActivity() {
     private val categories = listOf(
@@ -20,15 +22,18 @@ class ListActivity : AppCompatActivity() {
     private lateinit var categoryAdapter: CategoryAdapter
     private lateinit var rvTasks: RecyclerView
     private lateinit var taskAdapter: TaskAdapter
+    private lateinit var fabAddTask: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         initComponent()
         initUserInterface()
+        initListeners()
     }
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
     private fun initUserInterface() {
         categoryAdapter = CategoryAdapter(categories)
@@ -37,5 +42,15 @@ class ListActivity : AppCompatActivity() {
         taskAdapter = TaskAdapter(tasks)
         rvTasks.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rvTasks.adapter = taskAdapter
+    }
+    private fun initListeners() {
+        fabAddTask.setOnClickListener {
+            showDialog()
+        }
+    }
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_add_task)
+        dialog.show()
     }
 }
