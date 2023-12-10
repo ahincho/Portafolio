e00ca20eeab84d252f53d38164e9486461c8c5fb
ahincho <ahincho@unsa.edu.pe>
2023-12-09 22:03:07 -0500

    When Expression and Categories

--- Android/ToDoList/app/src/main/java/com/unsa/list/CategoryViewHolder.kt
+++ Android/ToDoList/app/src/main/java/com/unsa/list/CategoryViewHolder.kt
@@ -2,12 +2,32 @@ package com.unsa.list
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
     private val vwCategoryDivider: View = view.findViewById(R.id.vwCategoryDivider)
     fun render(category: Category) {
-        tvCategoryName.text = "Category"
+        when (category) {
+            Category.Business -> {
+                tvCategoryName.text = "Business"
+                vwCategoryDivider.setBackgroundColor(
+                    ContextCompat.getColor(vwCategoryDivider.context, R.color.business_category)
+                )
+            }
+            Category.Other -> {
+                tvCategoryName.text = "Other"
+                vwCategoryDivider.setBackgroundColor(
+                    ContextCompat.getColor(vwCategoryDivider.context, R.color.other_category)
+                )
+            }
+            Category.Personal -> {
+                tvCategoryName.text = "Personal"
+                vwCategoryDivider.setBackgroundColor(
+                    ContextCompat.getColor(vwCategoryDivider.context, R.color.personal_category)
+                )
+            }
+        }
     }
 }
