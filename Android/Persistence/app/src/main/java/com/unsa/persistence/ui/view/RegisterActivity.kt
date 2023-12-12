faf95bdbba142902b7ea88c515bffd3fafc70083
ahincho <ahincho@unsa.edu.pe>
2023-12-12 15:30:47 -0500

    Add User View and Activity

--- Android/Persistence/app/src/main/java/com/unsa/persistence/ui/view/RegisterActivity.kt
+++ Android/Persistence/app/src/main/java/com/unsa/persistence/ui/view/RegisterActivity.kt
@@ -3,10 +3,13 @@ package com.unsa.persistence.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.unsa.persistence.R
+import com.unsa.persistence.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- Android/Persistence/app/src/main/java/com/unsa/persistence/ui/viewmodel/LoginViewModel.kt
+++ Android/Persistence/app/src/main/java/com/unsa/persistence/ui/viewmodel/LoginViewModel.kt
@@ -1,10 +1,12 @@
 package com.unsa.persistence.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.unsa.persistence.data.model.User
 import com.unsa.persistence.data.providers.UserProvider
 
 class LoginViewModel : ViewModel() {
+    private val isLoading = MutableLiveData<Boolean>(true)
     fun login(user: User): Boolean {
         return UserProvider.login(user)
     }
