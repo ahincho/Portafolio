9d57f100ee77e6d1d335ae7cc8cf2c54e647eb4d
ahincho <ahincho@unsa.edu.pe>
2023-12-17 15:41:08 -0500

    Save Employee from Rest Controller

--- SpringBoot/testing/src/test/java/com/unsa/testing/controllers/EmployeeControllerTests.java
+++ SpringBoot/testing/src/test/java/com/unsa/testing/controllers/EmployeeControllerTests.java
@@ -1,11 +1,24 @@
 package com.unsa.testing.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unsa.testing.application.services.EmployeeService;
+import com.unsa.testing.domain.entities.Employee;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest
 public class EmployeeControllerTests {
@@ -15,4 +28,28 @@ public class EmployeeControllerTests {
     private EmployeeService employeeService;
     @Autowired
     private ObjectMapper objectMapper;
+    @Test
+    @DisplayName("Save Employee from Rest Controller")
+    void saveEmployeeTest() throws Exception {
+        // Given: Employee and service ready to go
+        Employee employee = Employee.builder()
+                .name("Angel")
+                .lastname("Hincho")
+                .email("ahincho@unsa.edu.pe")
+                .build();
+        given(employeeService.saveEmployee(any(Employee.class)))
+                .willAnswer((invocationOnMock) ->
+                    invocationOnMock.getArgument(0)
+                );
+        // When: Try to save employee using rest controller
+        ResultActions response = mockMvc.perform(post("/api/employee")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+        // Then: Check the response entity
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(employee.getName())))
+                .andExpect(jsonPath("$.lastname", is(employee.getLastname())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
 }
