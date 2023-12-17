6f8857841eab113589fdeb0f35b95e85653d00e0
ahincho <ahincho@unsa.edu.pe>
2023-12-17 08:39:28 -0500

    Recover All Employees from Database Test

--- SpringBoot/testing/src/test/java/com/unsa/testing/repositories/EmployeeRepositoryTests.java
+++ SpringBoot/testing/src/test/java/com/unsa/testing/repositories/EmployeeRepositoryTests.java
@@ -4,13 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.unsa.testing.domain.entities.Employee;
 import com.unsa.testing.domain.repositories.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTests {
+    private Employee employee;
+    @BeforeEach
+    void setup() {
+        System.out.println("Test started!");
+        employee = Employee.builder()
+                .name("Angel")
+                .lastname("Hincho")
+                .email("ahincho@unsa.edu.pe")
+                .build();
+    }
     @Autowired
     private EmployeeRepository employeeRepository;
     // Given: Precondition or configuration
@@ -30,5 +43,22 @@ public class EmployeeRepositoryTests {
         // Then: Verify the id field
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isGreaterThan(0L);
+    }
+    @Test
+    @DisplayName("List Employees on Database Test")
+    void listEmployeesTest() {
+        // Given: Employees saved on Database
+        Employee anotherEmployee = Employee.builder()
+                .name("Angel")
+                .lastname("Hincho")
+                .email("ahincho@unsa.edu.pe")
+                .build();
+        employeeRepository.save(employee);
+        employeeRepository.save(anotherEmployee);
+        // When: Recover employees from Database
+        List<Employee> employees = employeeRepository.findAll();
+        // Then: Verify records on Database
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
     }
 }
