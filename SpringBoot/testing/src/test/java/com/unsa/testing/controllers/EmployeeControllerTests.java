6aa8128f4b94239dd307b42c3fb945ef24aaa775
ahincho <ahincho@unsa.edu.pe>
2023-12-17 14:22:36 -0500

    Employee Rest Controller Tests

---
+++ SpringBoot/testing/src/test/java/com/unsa/testing/controllers/EmployeeControllerTests.java
@@ -0,0 +1,18 @@
+package com.unsa.testing.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.unsa.testing.application.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class EmployeeControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private EmployeeService employeeService;
+    @Autowired
+    private ObjectMapper objectMapper;
+}
