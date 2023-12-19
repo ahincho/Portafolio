fdf3907f1e8ed79495cd50032e3e40a40c5ab80a
ahincho <ahincho@unsa.edu.pe>
2023-12-19 17:59:05 -0500

    Quote Dao

---
+++ Android/Quotations/app/src/main/java/com/unsa/quotations/data/database/daos/QuoteDao.kt
@@ -0,0 +1,15 @@
+package com.unsa.quotations.data.database.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.unsa.quotations.data.database.entities.QuoteEntity
+
+@Dao
+interface QuoteDao {
+    @Query("SELECT * FROM quotes")
+    suspend fun getAllQuotes(): List<QuoteEntity>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(quotes: List<QuoteEntity>)
+}
