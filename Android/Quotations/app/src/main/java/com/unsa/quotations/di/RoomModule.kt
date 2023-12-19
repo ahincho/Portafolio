84111c4c9e648529f7eeab3bc7d345ff74cea39f
ahincho <ahincho@unsa.edu.pe>
2023-12-19 18:04:29 -0500

    Room Module with Dagger Hilt

---
+++ Android/Quotations/app/src/main/java/com/unsa/quotations/di/RoomModule.kt
@@ -0,0 +1,24 @@
+package com.unsa.quotations.di
+
+import android.content.Context
+import androidx.room.Room
+import com.unsa.quotations.data.database.QuoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+    private const val QUOTE_DATABASE_NAME = "quote_db"
+    @Singleton
+    @Provides
+    fun provideRoom(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, QuoteDatabase::class.java, QUOTE_DATABASE_NAME).build()
+    @Singleton
+    @Provides
+    fun provideQuoteDao(database: QuoteDatabase) = database.getQuoteDao()
+}
