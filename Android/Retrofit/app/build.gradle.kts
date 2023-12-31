1000e11b95c22633b7c8e745278bd62e4a76c8a9
ahincho <ahincho@unsa.edu.pe>
2023-12-31 17:11:03 -0500

    Picasso and Retrofit Dependencies

--- Android/Retrofit/app/build.gradle.kts
+++ Android/Retrofit/app/build.gradle.kts
@@ -36,11 +36,17 @@ android {
 }
 
 dependencies {
-
+    // Core Dependencies
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Picasso Dependency
+    implementation("com.squareup.picasso:picasso:2.71828")
+    // Retrofit Dependency
+    implementation("com.squareup.retrofit2:retrofit2:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Testing Dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
