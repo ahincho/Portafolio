be276d179bf6c57b4bc62d47b3ab8674c7187d73
ahincho <ahincho@unsa.edu.pe>
2023-12-31 20:19:33 -0500

    Upgrade to SDK 34

--- Android/DataStore/app/build.gradle.kts
+++ Android/DataStore/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.ahincho.data.store"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ahincho.data.store"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
